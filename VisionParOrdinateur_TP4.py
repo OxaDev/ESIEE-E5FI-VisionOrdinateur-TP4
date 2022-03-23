da7f006adeeb6b57f8e8875fea7fa937de413443
Oxasa <encoder.onbekend@gmail.com>
2022-03-23 14:17:52 +0100

    SVC et KDA (lol)

--- VisionParOrdinateur_TP4.py
+++ VisionParOrdinateur_TP4.py
@@ -1,7 +1,8 @@
 import cv2
-import pymld
+from dml import kda
 from os import listdir, system, name
 from os.path import isfile, join
+from sklearn import svm
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import numpy as np
@@ -26,7 +27,6 @@ def getDescritors(chemins):
     desc = np.reshape(descriptors, (len(descriptors)//64, 64))
     desc = np.float32(desc)
     return desc
-
 
 def vocabulaire(N, desc):
 
@@ -90,6 +90,57 @@ def get_model(n_cluster):
     k_means = pickle.load(open(filename, 'rb'))
     return k_means
 
+def svc_application(classes):
+    filename_filenames = join('part2_saves','base_im_filenames_N_'+str(Nb_cluster)+'.pickle')
+    filenames = pickle.load(open(filename_filenames, 'rb'))
+    filenames_filtered = []
+    filename_vectors = join('part2_saves','base_im_vectors_N_'+str(Nb_cluster)+'.pickle')
+    vectors = pickle.load(open(filename_vectors, 'rb'))
+
+    mat_X = []
+    vect_Y = []
+    print(" -- Lecture des données -- ")
+    for i in range(len(vectors)):
+        current_vector = vectors[i]
+        current_filename = filenames[i]
+
+        for i_class in range(len(classes)):
+            name_class = classes[i_class]
+            if(name_class in current_filename):
+                filenames_filtered.append(current_filename)
+                vect_Y.append(i_class+1)
+                vect_temp = []
+                for i_cluster in range(Nb_cluster):
+                    vect_temp.append(0)
+                
+                for elem in current_vector:
+                    vect_temp[elem] += 1
+
+                mat_X.append(vect_temp)
+                break
+    print(" -- Fin de la lecture -- ")
+
+    print(" -- Création du model SVC et Fit des données sur les 2 répertoires -- ")
+    svc_model = svm.SVC()
+    svc_model.fit(mat_X,vect_Y)
+
+    print(" -- Predict des données avec le SVC -- ")
+    predic_svc=  svc_model.predict(mat_X)
+    print(" -- Predict du SVC : " + str(predic_svc) + " -- ")
+
+    nb_error = 0
+    nb_success = 0
+    for i in range(len(predic_svc)):
+        current_predict = predic_svc[i]
+        current_filename = filenames_filtered[i]
+        current_predicted_name = classes[current_predict-1]
+        #print("-- For filename : " + str(current_filename)+ " -- predicted : " + str(current_predicted_name))
+        if(current_predicted_name in current_filename):
+            nb_success +=1
+        else:
+            nb_error += 1
+    print(" -- Nombre de succès de prédictions : " + str(nb_success) + " -- Nombre d'erreur : " + str(nb_error))     
+
 ####### Variables #######
 list_dir = [
     join("101_ObjectCategories","ant"),
@@ -97,7 +148,14 @@ list_dir = [
     join("101_ObjectCategories","cougar_face"),
     join("101_ObjectCategories","garfield")
 ]
+mode = "saveKmeans"
+mode = "vocabulaire"
 mode = "vectorisation"
+mode = "Appr_Et_Test_KDA"
+mode = "Appr_Et_Test_SVC_2"
+#mode = "Appr_Et_Test_SVC_4"
+
+Nb_cluster = 512
 
 ######### Execution #########
 if(mode== "saveKmeans"):
@@ -116,32 +174,6 @@ if(mode== "saveKmeans"):
 
         end = time.time()
         print("saved -- time : " + str(end - start) + "s")
-
-
-if(mode == "vectorisation"):
-    im_vectors = np.asarray([])
-    im_filename = []
-    Nb_cluster = 512
-
-    k_means = get_model(Nb_cluster)
-
-    for path in list_dir:
-        onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
-        indic = 1
-        for filename in onlyfiles:
-            im = join(str(path),str(filename))
-            vector_im = vectoriser(im, k_means)
-
-            im_vectors = np.append(im_vectors, vector_im[0])
-            im_filename.append(im)
-            indic +=1
-
-    with open(join('part2_saves','base_im_vectors_N_'+str(Nb_cluster)+'.pickle'), 'wb') as f:
-        pickle.dump(im_vectors, f)
-    with open(join('part2_saves','base_im_filenames_N_'+str(Nb_cluster)+'.pickle'), 'wb') as f:
-        pickle.dump(im_filename, f)
-    
-    print("-- Vectorisation file saved --")
 
 if(mode == "vocabulaire"):
     desc = getDescritors(list_dir)
@@ -171,3 +203,106 @@ if(mode == "vocabulaire"):
     plt.ioff()
     plt.savefig(join("part1_saves","list_error_max.png") )
 
+if(mode == "vectorisation"):
+    im_vectors = []
+    im_filename = []
+
+    k_means = get_model(Nb_cluster)
+
+    for path in list_dir:
+        onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
+        indic = 1
+        for filename in onlyfiles:
+            im = join(str(path),str(filename))
+            vector_im = vectoriser(im, k_means)
+
+            im_vectors.append(vector_im)
+            im_filename.append(im)
+            indic +=1
+
+    with open(join('part2_saves','base_im_vectors_N_'+str(Nb_cluster)+'.pickle'), 'wb') as f:
+        pickle.dump(im_vectors, f)
+    with open(join('part2_saves','base_im_filenames_N_'+str(Nb_cluster)+'.pickle'), 'wb') as f:
+        pickle.dump(im_filename, f)
+    
+    print("-- Vectorisation file saved --")
+
+if(mode == "Appr_Et_Test_KDA"):
+    filename_filenames = join('part2_saves','base_im_filenames_N_'+str(Nb_cluster)+'.pickle')
+    filenames = pickle.load(open(filename_filenames, 'rb'))
+
+    filename_vectors = join('part2_saves','base_im_vectors_N_'+str(Nb_cluster)+'.pickle')
+    vectors = pickle.load(open(filename_vectors, 'rb'))
+
+    mat_X = []
+    vect_Y = []
+    classes = ['cougar_face','garfield']
+    print(" -- Lecture des données -- ")
+    for i in range(len(vectors)):
+        current_vector = vectors[i]
+        current_filename = filenames[i]
+
+        for i_class in range(len(classes)):
+            name_class = classes[i_class]
+            if(name_class in current_filename):
+                vect_Y.append(i_class+1)
+                vect_temp = []
+                for i_cluster in range(Nb_cluster):
+                    vect_temp.append(0)
+                
+                for elem in current_vector:
+                    vect_temp[elem] += 1
+
+                mat_X.append(vect_temp)
+                break
+    print(" -- Fin de la lecture -- ")
+
+    ## Test KDA ##
+
+    X=np.array([[1,2],[1,3],[2,1],[7,8],[9,8]])
+    y=[1,1,1,-1,-1]
+    polynomes_deg = [1,2,3,4,5,6,7,8,9,10]
+    values_transform=[]
+
+    for poly in polynomes_deg:
+        s= kda.KDA(n_components=2, kernel='poly', degree=poly)
+        s.fit(X,y)
+        temp_value = s.transform(X)
+        values_transform.append(temp_value)
+
+        #print(str("\nDegré polynome : " + str(poly) + " -- Valeurs transform : " + str(temp_value)))
+
+    ##############
+    """
+    Plus le degré du polynome est élevé plus les valeurs seront éloignées entre les deux classes, 
+    """
+
+    print(" -- KDA sur la matrice X et le vecteur Y -- ")
+    values_transform=[]
+    for poly in polynomes_deg:
+        s= kda.KDA(n_components=2, kernel='poly', degree=poly)
+        s.fit(mat_X,vect_Y)
+        temp_value = s.transform(mat_X)
+        values_transform.append(temp_value)
+        print(str("\nDegré polynome : " + str(poly) + " -- Valeurs transform : " + str(temp_value)))
+    print(" -- Fin du KDA -- ")
+    '''
+    Les projections semblent montrer une bonne séparation des classes lorsque le polynome est élevé ( > à 7 ) 
+    car on remarquer que certaines valeurs sont positives et d'autres négatives avec un nombre élevé.
+    '''
+    
+if(mode == "Appr_Et_Test_SVC_2"):
+    classes = ['cougar_face','garfield']
+    svc_application(classes)
+    '''
+    Résultats plutôt corrects, 1 seule erreur reportée avec la prédiction du modèle SVC 
+    '''
+
+if(mode == "Appr_Et_Test_SVC_4"):
+
+    classes = ['cougar_face','garfield','ant','camera']
+    svc_application(classes)
+    '''
+    Résultats plutôt corrects, 2 erreurs reportées avec la prédiction du modèle SVC 
+    '''
+        
